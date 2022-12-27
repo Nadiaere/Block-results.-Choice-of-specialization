f8bdd7df78d1b4f0901525bc109149eac3212c5c
Nadiaere <knw_@list.ru>
2022-12-28 00:46:42 +0500

    Решение задачи

--- Lesson10/Task10/Program.cs
+++ Lesson10/Task10/Program.cs
@@ -4,3 +4,36 @@
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 Console.WriteLine("Hello, world!");
+
+string[] array1 = new string[] {"Hello","2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Новый массив из строк");
+SecondArray(array1, array2);
+PrintArray(array2);
+
+
+
+
